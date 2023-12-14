b0a842984ff2158bf2924bbf497b42a319f4aba4
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2023-12-14 16:10:11 +0900

    d

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -21,7 +21,7 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
-    XHR.onload = () => {
+    XHR.onload = () => {f
       if(XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
